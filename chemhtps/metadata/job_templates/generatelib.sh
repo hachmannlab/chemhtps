0e2fe026367ac6698e0725dbad61ce9c49a00d9e
mojtaba <mojtabah@buffalo.edu>
2019-05-13 13:01:07 -0400

    moved job tmeplates to here

---
+++ chemhtps/metadata/job_templates/generatelib.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+Timehere
+Nodeshere
+#SBATCH --job-name="generatelib"
+#SBATCH --output=generatelib.out
+Cpushere
+
+# ====================================================
+# For 16-core nodes
+# ====================================================
+##SBATCH --constraint=CPU-E5-2630v3
+##SBATCH --cpus-per-task=16
+#SBATCH --mem=64000
+
+echo "SLURM job ID         = "$SLURM_JOB_ID
+echo "Working Dir          = "$SLURM_SUBMIT_DIR
+echo "Temporary scratch    = "$SLURMTMPDIR
+echo "Compute Nodes        = "$SLURM_JOB_NODELIST
+echo "Number of Processors = "$SLURM_NPROCS
+echo "Number of Nodes      = "$SLURM_JOB_NUM_NODES
+echo "Tasks per Node       = "$SLURM_TASKS_PER_NODE
+echo "Memory per Node      = "$SLURM_MEM_PER_NODE
+
+ulimit -s unlimited
+#module load python
+#module load intel-mpi
+#conda install -c openbabel openbabel
+#source activate my_chemlg_env
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/user/m27/pkg/openbabel/2.3.2/lib
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+
+echo "Launch job"
+Runlinehere
+echo "All Done!"
